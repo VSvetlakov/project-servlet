8c64e51c91efedb1810cfb98a4bd7f867fc2cad2
VSvetlakov <116279684+VSvetlakov@users.noreply.github.com>
2023-10-03 22:31:06 +0400

    ver 3

--- src/main/java/com/tictactoe/InitServlet.java
+++ src/main/java/com/tictactoe/InitServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 
 @WebServlet(name = "InitServlet", value = "/start")
